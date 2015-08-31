79a769ed02b51f7ac9bed54fe957c9ab04a533e6
Niklas Rosenstein <rosensteinniklas@gmail.com>
2015-08-31 17:59:56 +0200

    added scripts and updated setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 # All rights reserved.
 
 from setuptools import setup
+import os
+
+script = 'scripts/craftr.py' if os.name == 'nt' else 'scripts/craftr'
 
 setup(
   name='craftr-build',
@@ -9,4 +12,5 @@ setup(
   author='Niklas Rosenstein',
   author_email='rosensteinniklas(at)gmail.com',
   install_requires=['glob2', 'colorama'],
+  scripts=[script],
 )
