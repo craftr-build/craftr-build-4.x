f5f9cafa7cd3381a343346d792752866846ee2af
Niklas Rosenstein <rosensteinniklas@gmail.com>
2015-09-06 15:42:04 +0200

    Fixed setup.py to actually register the craftr package and its package data

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2015 Niklas Rosenstein
 # All rights reserved.
 
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 script = 'scripts/craftr.py' if os.name == 'nt' else 'scripts/craftr'
@@ -11,6 +11,12 @@ setup(
   version='0.0.9',
   author='Niklas Rosenstein',
   author_email='rosensteinniklas(at)gmail.com',
+  url='https://github.com/craftr-build/craftr',
   install_requires=['glob2', 'colorama'],
   scripts=[script],
+  packages=find_packages('.'),
+  package_dir={'': '.'},
+  package_data={
+    'craftr': ['builtins/*.craftr']
+  },
 )
