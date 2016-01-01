679319da943425b54f0a9bb85bead43e9010a03d
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-01-01 20:05:19 +0100

    changed setup.py and bin/craftr scripts, on Windows, renamed scripts with .py suffix will now be created from the unix scripts

--- setup.py
+++ setup.py
@@ -3,19 +3,33 @@
 
 from functools import partial
 from setuptools import setup, find_packages
+
 import os
 import pip.req, pip.download
+import shutil
 import sys
 
 if sys.version < '3.4':
   sys.exit("Craftr requires Python 3.4 or greater.")
 
+# parse_requirements() interface has changed in Pip 6.0
 from pip.req import parse_requirements
 if pip.__version__ >= '6.0':
   parse_requirements = partial(parse_requirements, session=pip.download.PipSession())
 
-requires = [str(x.req) for x in parse_requirements('requirements.txt')]
-script = 'bin/craftr.py' if os.name == 'nt' else 'bin/craftr'
+scripts = ['bin/craftr']
+
+# On Windows, we need this scripts with a .py suffix.
+if os.name == 'nt':
+  if not os.path.isdir('build/bin'):
+    os.makedirs('build/bin')
+  new_scripts = [os.path.join('build', x) + '.py' for x in scripts]
+  for src, dst in zip(scripts, new_scripts):
+    shutil.copy2(src, dst)
+  scripts = new_scripts
+
+# Parse the requirements from requirements.txt
+requirements = [str(x.req) for x in parse_requirements('requirements.txt')]
 
 setup(
   name='craftr-build',
@@ -23,8 +37,8 @@ setup(
   author='Niklas Rosenstein',
   author_email='rosensteinniklas(at)gmail.com',
   url='https://github.com/craftr-build/craftr',
-  install_requires=requires,
-  scripts=[script],
+  install_requires=requirements,
+  scripts=scripts,
   packages=find_packages('.'),
   package_dir={'': '.'},
   package_data={
