cb56bda4f6149940e78130364a1b904d3b550797
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-01-15 01:53:23 +0100

    add craftr.utils.slotobject() factory function

--- craftr/utils.py
+++ craftr/utils.py
@@ -23,6 +23,7 @@ from craftr import environ, path
 import os
 import sys
 
+# PATH fiddling --------------------------------------------------------------
 
 def append_path(pth):
   ''' Append *pth* to the `PATH` environment variable. '''
@@ -72,3 +73,64 @@ def find_program(name):
       if path.isfile(extname) and os.access(extname, os.X_OK):
         return extname
   raise FileNotFoundError(name)
+
+
+
+# General Programming Utilities ----------------------------------------------
+
+def slotobject(cls_name, slots):
+  ''' Similar to :func:`collections.namedtuple`, this function creates
+  a type object with the attributes specified via the *slots* (a string
+  or list of strings) only the type does not inherit from :class:`tuple`
+  but insead from :class:`object`. Its attributes can actually be changed.
+  Instances of the type can not act as tuples.
+
+  .. code-block:: python
+
+    >>> from craftr.utils import slotobject
+    >>> File = slotobject('File', 'name arc_name')
+    >>> File('foo', 'subdir/foo')
+    <File name='foo' arc_name='subdir/foo'>
+  '''
+
+  if isinstance(slots, str):
+    if ',' in slots:
+      slots = slots.split(',')
+    else:
+      slots = slots.split(' ')
+  slots_set = frozenset(slots)
+  assert len(slots_set) == len(slots)
+
+  def __init__(self, *args, **kwargs):
+    # Fill the args matching the slots in kwargs.
+    if len(args) > len(slots):
+      raise TypeError('{0}() expected {1} positional arguments, got {2}'.format(
+        cls_name, len(slots), len(args)))
+    for key in kwargs:
+      if key not in slots_set:
+        raise TypeError('{0}() unexpected keyword argument \'{1}\''.format(
+          cls_name, key))
+    for key, arg in zip(slots, args):
+      if key in kwargs:
+        raise TypeError('{0}() duplicate parameter for argument \'{1}\''.format(
+          cls_name, key))
+      kwargs[key] = arg
+    if len(kwargs) != len(slots):
+      raise TypeError('{0}() missing arguments'.format(cls_name))
+    for key, value in kwargs.items():
+      setattr(self, key, value)
+
+  def __str__(self):
+    parts = []
+    for key in slots:
+      parts.append('{0}={1!r}'.format(key, getattr(self, key)))
+    result = '<{0}'.format(cls_name)
+    if parts:
+      result += ' ' + ' '.join(parts)
+    return result + '>'
+
+  return type(cls_name, (object,), {
+    '__init__': __init__,
+    '__slots__': slots,
+    '__str__': __str__,
+  })
