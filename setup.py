5538c6c9a87ee509eb2197b5b6d444116421fc39
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-07-19 02:51:08 +0200

    v1.1.1

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ requirements = [str(x.req) for x in parse_requirements('requirements.txt')]
 
 setup(
   name = 'craftr-build',
-  version = '1.1.1.dev.0',
+  version = '1.1.1',
   description = 'next generation build system based on Ninja and Python',
   long_description = long_description,
   classifiers = [
