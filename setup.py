72ff4c0f221f40292285a21f97130dff95587576
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-08-09 14:53:50 +0200

    update requirements.txt with nr.utils==1.0, add .pubfile

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ requirements = [str(x.req) for x in parse_requirements('requirements.txt')]
 
 setup(
   name = 'craftr-build',
-  version = '1.1.2.dev',
+  version = '1.1.2',
   description = 'next generation build system based on Ninja and Python',
   long_description = long_description,
   classifiers = [
