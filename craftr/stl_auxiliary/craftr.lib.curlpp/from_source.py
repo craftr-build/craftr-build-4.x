b25d5863378ae1a9ade3adbfb308d2f4eb76343d
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-12-01 23:52:54 +0100

    use std=gnu++11 for dlib in non-Windows platforms (not 100% sure about mac yet) and fix curl/curlpp source compilation missing the options name

--- craftr/stl_auxiliary/craftr.lib.curlpp/from_source.py
+++ craftr/stl_auxiliary/craftr.lib.curlpp/from_source.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+options = session.module.options
 from craftr.loaders import external_archive
 
 source_directory = external_archive(
