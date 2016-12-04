cc94a5440eb3d7b31ab8e5f516a74880f040a309
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-12-04 20:03:24 +0100

    Prepare 2.0.0.dev4 release

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ else:
 
 setup(
   name = 'craftr-build',
-  version = '2.0.0.dev3',
+  version = '2.0.0.dev4',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   description = 'Meta build system based on Ninja and Python',
