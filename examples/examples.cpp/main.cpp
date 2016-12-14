1d391040a4348163727ca06c6b3ffb1f77d73dc5
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-12-14 20:01:40 +0100

    update missing license headers in example files

--- examples/examples.cpp/main.cpp
+++ examples/examples.cpp/main.cpp
@@ -1,5 +1,5 @@
-/**
- * Copyright (C) 2016  Niklas Rosenstein
+/* The Craftr build system
+ * Copyright (C) <year>  <name of author>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,9 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 #include <iostream>
 #include <sstream>
