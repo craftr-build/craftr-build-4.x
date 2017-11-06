1ea4d0fb5d48aa4b2d564d4f060da3c08c7bd948
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-11-06 16:12:07 +0100

    add java build example

---
+++ examples/java/Craftrfile.py
@@ -0,0 +1,20 @@
+
+import craftr from 'craftr'
+import java from 'craftr/lang/java'
+
+java.prebuilt(
+  name = 'hello',
+  binary_jar = 'vendor/hello-1.0.0.jar'
+)
+
+java.library(
+  name = 'main_lib',
+  srcs = craftr.glob('src/**.java'),
+  transitive_deps = [':hello']
+)
+
+java.binary(
+  name = 'main',
+  deps = [':main_lib'],
+  main_class = 'Main'
+)
