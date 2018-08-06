27faafafad0f95814709bbd5b6e2457464f242b9
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-08-06 16:52:05 +0200

    add setup.py, requirements.txt, MANIFEST.in and .gitignore

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+import io
+import setuptools
+
+with io.open('requirements.txt') as fp:
+  requirements = fp.readlines()
+
+setuptools.setup(
+  name = 'craftr-build',
+  version = '4.0.0.dev0',
+  author = 'Niklas Rosenstein',
+  author_email = 'rosensteinniklas@gmail.com',
+  license = 'MIT',
+  url = 'https://github.com/craftr-build/craftr',
+  packages = setuptools.find_packages('src'),
+  package_dir = {'': 'src'},
+  entry_points = {
+    'console_scripts': ['craftr=craftr.main:main']
+  },
+  install_requires = requirements
+)
