56e7a158d8ff87c493bc7a9308a1887bc62ae471
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-04-03 02:12:29 +0200

    import Action type into craftr.core module

--- craftr-core/src/craftr/core/__init__.py
+++ craftr-core/src/craftr/core/__init__.py
@@ -2,6 +2,7 @@
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
 __version__ = '0.0.0'
 
+from craftr.core.actions import Action
 from craftr.core.closure import Closure
 from craftr.core.property import HavingProperties, Property
 from craftr.core.system import Context, Project, Task
