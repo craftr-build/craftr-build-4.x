6fd8cef3af2cc6971def1d95761f74e427bee7e5
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-04-05 12:35:33 +0200

    update cli

--- craftr-cli/src/craftr/cli/main.py
+++ craftr-cli/src/craftr/cli/main.py
@@ -5,8 +5,6 @@ from pathlib import Path
 from craftr.core.context import Context
 from craftr.core.settings import Settings
 
-PYTHON_BUILD_SCRIPT = 'build.craftr.py'
-
 parser = argparse.ArgumentParser()
 parser.add_argument('-O', '--option', default=[], action='append',
   help='Set or override an option in the settings of the build.')
@@ -15,16 +13,12 @@ parser.add_argument('--settings-file', default=Context.CRAFTR_SETTINGS_FILE, typ
 parser.add_argument('tasks', metavar='task', nargs='*')
 
 
-def main() -> None:
+def main():
   args = parser.parse_args()
   settings = Settings.from_file(Path(args.settings_file))
   settings.update(Settings.parse(args.option))
   context = Context(settings)
-  project = context.project(Path.cwd())
-
-  filename = PYTHON_BUILD_SCRIPT
-  scope = {'project': project, '__file__': filename, '__name__': '__main__'}
-  exec(compile(Path(filename).read_text(), filename, 'exec'), scope, scope)
+  context.load_project(Path.cwd())
 
   context.execute(args.tasks or None)
 
