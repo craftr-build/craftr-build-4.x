dac350d5d017d1996083df6919c5e28bd9f2db53
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-08-27 02:02:40 +0200

    add ocaml example

---
+++ craftr-build/src/craftr/build/plugins/ocaml.py
@@ -0,0 +1,59 @@
+
+"""
+Provides a simple interface to building OCaml applications.
+"""
+
+import os
+import typing as t
+import typing_extensions as te
+from craftr.build.lib.helpers import TaskFactoryExtension
+
+from craftr.core import Project, Property, Task
+from craftr.core.actions import CreateDirectoryAction, CommandAction
+from craftr.core.actions.action import Action
+
+
+class OcamlApplication(Task):
+
+  output_file: te.Annotated[Property[str], Task.Output]
+  srcs: te.Annotated[Property[t.List[str]], Task.InputFile]
+  standalone: Property[bool]
+
+  # Properties that construct the output filename.
+  output_directory: Property[str]
+  product_name: Property[str]
+  suffix: Property[str]
+
+  def init(self) -> None:
+    self.standalone.set_default(lambda: True)
+    self.output_directory.set_default(lambda: os.path.join(self.project.build_directory, 'ocaml', self.name))
+    self.product_name.set_default(lambda: 'main')
+    self.suffix.set_default(lambda: '.exe' if (self.standalone.get() and os.name == 'nt') else '.cma' if self.standalone.get() else '')
+    self.output_file.set_default(lambda: os.path.join(self.output_directory.get(), self.product_name.get() + self.suffix.get()))
+
+    self.run = self.project.task(self.name + 'Run')
+    self.run.group = 'run'
+    self.run.default = False
+    self.run.depends_on(self)
+
+  def finalize(self) -> None:
+    super().finalize()
+    self.run.do_last(CommandAction([self.output_file.get()]))
+
+  def get_actions(self) -> t.List['Action']:
+    command = ['ocamlopt' if self.standalone.get() else 'ocamlc']
+    command += ['-o'] + [self.output_file.get()] + self.srcs.get()
+
+    # TODO(nrosenstein): Add cleanup action to remove .cmi/cmx/.o files?
+    #   There doesn't seem to be an option in the Ocaml compiler to change their
+    #   output location.
+
+    return [
+      CreateDirectoryAction(os.path.dirname(self.output_file.get())),
+      CommandAction(command),
+    ]
+
+
+def apply(project: Project, name: str) -> None:
+  project.add_extension('OcamlApplication', OcamlApplication)
+  project.add_extension('ocamlApplication', TaskFactoryExtension(project, 'ocamlApplication', OcamlApplication))
